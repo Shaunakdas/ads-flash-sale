2929e334f26fd77bca70c737d5ca28cb78387799
Shaunak Das <shaunak.das@tokopedia.com>
2018-08-28 11:45:39 +0530

    Updated Header Component and logon png files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,16 +25,11 @@ class Header extends React.Component {
             <img
               src={logoUrl}
               srcSet={`${logoUrl2x} 2x`}
-              width="38"
-              height="38"
+              height="30"
               alt="React"
             />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>TopAds</span>
           </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
         </div>
       </div>
     );
