1fceb153af11fae8080c4d78bdab32b1fda4c838
Shaunak Das <shaunak.das@tokopedia.com>
2018-08-29 16:06:24 +0530

    Added HeaderBox Bottom

--- src/routes/sales/components/HeaderBox/index.js
+++ src/routes/sales/components/HeaderBox/index.js
@@ -34,7 +34,43 @@ class HeaderBox extends React.Component {
             
           </div>
           <div className={s.divider}></div>
-          <div className={s.bottom}></div>
+          <div className={s.bottom}>
+            <div className={s.campaignPropItem}>
+              <div className={s.campaignPropItemTop}>
+                {'Periode Berlangsung'}
+              </div>
+              <div className={s.campaignPropItemBottom}>
+                {'23 Mar, 09.00 - 23 Mar 2018, 10.00 WIB'}
+              </div>
+            </div>
+            <div className={s.campaignPropItem}>
+              <div className={s.campaignPropItemTop}>
+                {'Batas Pendaftaran'}
+              </div>
+              <div className={s.campaignPropItemBottom}>
+                {'15 Mar 2018, 15.00'}
+              </div>
+            </div>
+
+            <div className={s.campaignPropItem}>
+              <div className={s.campaignPropItemTop}>
+                {'Merchant Terdaftar'}
+              </div>
+              <div className={s.campaignPropItemBottom}>
+                {'1.532'}
+              </div>
+            </div>
+
+            <div className={s.campaignPropItem}>
+              <div className={s.campaignPropItemTop}>
+                {'Produk Terdaftar'}
+              </div>
+              <div className={s.campaignPropItemBottom}>
+                {'2.334'}
+              </div>
+            </div>
+            
+          </div>
           
         </div>
       </div>
