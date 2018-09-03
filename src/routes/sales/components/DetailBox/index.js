d3fe30094f13ca6db9dffe63de43f812f90b6e5a
Shaunak Das <shaunak.das@tokopedia.com>
2018-09-03 12:22:34 +0530

    Added Tab and TabContent into sales page

--- src/routes/sales/components/DetailBox/index.js
+++ src/routes/sales/components/DetailBox/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Tab from 'unify-react-mobile/build/Tab';
+import TabContent from 'unify-react-mobile/build/TabContent';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './DetailBox.css';
 import ProductListTab from './components/ProductListTab';
+import CategoryInfoTab from './components/CategoryInfoTab';
 
 class DetailBox extends React.Component {
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <ProductListTab />
+          <Tab
+            secondary
+            items={[
+              { key: 0, text: 'Deskripsi & Kriteria' },
+              { key: 1, text: 'Daftar Produk' },
+            ]}
+            indexActive={0}
+          >
+            <TabContent>
+              <CategoryInfoTab />
+            </TabContent>
+            <TabContent>
+              <ProductListTab />
+            </TabContent>
+          </Tab>
         </div>
       </div>
     );
